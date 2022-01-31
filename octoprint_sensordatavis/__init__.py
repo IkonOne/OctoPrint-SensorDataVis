4d608f064682dce3af7d1f907a099d07527818ca
Erin M. Gunn <ikonone@gmail.com>
2022-01-31 14:23:45 -0700

    Fixed streaming so it is always on.

--- octoprint_sensordatavis/__init__.py
+++ octoprint_sensordatavis/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from flask import json, jsonify, request
 import octoprint_sensordatavis.arduino as arduino
 import octoprint_sensordatavis.lims as lims
+import octoprint_sensordatavis.data_collector as data_collector
 import itertools
 import numpy as np
 
@@ -21,8 +22,26 @@ class SensordatavisPlugin(
     octoprint.plugin.SettingsPlugin,
     octoprint.plugin.TemplatePlugin,
     octoprint.plugin.BlueprintPlugin,
-    octoprint.plugin.EventHandlerPlugin
+    octoprint.plugin.EventHandlerPlugin,
+    octoprint.plugin.StartupPlugin
 ):
+
+    ##-- StartupPlugin mixin
+
+    def on_after_startup(self):
+        data_collector._logger = self._logger
+        
+        lims_ip = self._settings.get(["lims_ip"])
+        lims_port = self._settings.get(["lims_port"])
+        lims_endpoint = self._settings.get(["lims_endpoint"])
+        lims.start_streaming(lims_ip, lims_port, lims_endpoint, self._logger)
+
+        port = self._settings.get(["arduino_port"])
+        baud = self._settings.get(["arduino_baud"])
+        sensors = self._settings.get(["arduino_sensors"])
+        arduino.start_streaming(port, baud, lims.msgQueue, sensors, self._logger)
+
+
     ##-- AssetPlugin mixin
 
     # def get_assets(self):
@@ -73,22 +92,6 @@ class SensordatavisPlugin(
 
     def on_event(self, event, payload):
         # printer states: https://docs.octoprint.org/en/master/modules/printer.html#octoprint.printer.PrinterInterface.get_state_id
-        if event == 'PrinterStateChanged':
-            self._logger.debug('Printer State : {1}'.format(event, payload))
-            state_id = payload['state_id']
-            if state_id == 'STARTING':
-                lims_ip = self._settings.get(["lims_ip"])
-                lims_port = self._settings.get(["lims_port"])
-                lims_endpoint = self._settings.get(["lims_endpoint"])
-                lims.start_streaming(lims_ip, lims_port, lims_endpoint, self._logger)
-
-                port = self._settings.get(["arduino_port"])
-                baud = self._settings.get(["arduino_baud"])
-                sensors = self._settings.get(["arduino_sensors"])
-                arduino.start_streaming(port, baud, lims.msgQueue, sensors, self._logger)
-            if state_id in ['OPERATIONAL', 'CANCELLLING']:
-                arduino.stop_streaming()
-                lims.stop_streaming()
         
         if event == 'plugin_bedlevelvisualizer_mesh_data_collected':
             # LOLOLO : Did you know that Events.ALL_CAPS_EVENT_NAMES != 'ALL_CAPS_EVENT_NAMES'
--- octoprint_sensordatavis/lims.py
+++ octoprint_sensordatavis/lims.py
@@ -27,36 +27,13 @@ def stream_loop():
         
         time.sleep(1)
 
-        values_to_set = dict()
+        # values_to_set = dict()
         # sensors = data_collector.get_summarized_readings()
         values_to_set = data_collector.get_summarized_readings()
-        _lims.logger.debug(f'Sending data to lims: {values_to_set}')
-        # for key in sensors.keys():
-        #     sensor = sensors[key]
-        #     _lims.logger.debug(f'WTF is sensor???:\nkey: {key}\nsensor: {sensor}')
-        #     for metric in sensor:
-        #         values_to_set[key + '.' + metric] = sensor[metric]
-        
+        _lims.logger.debug(f'[LIMS] Sending data to lims: {values_to_set}')
+
         if not _lims.engine.set_current_data_values(values_to_set):
             _lims.logger.debug(f'[LIMS] Failed to set values...\n{values_to_set}')
-
-        while not msgQueue.empty:
-            msg = msgQueue.get()
-            values_to_set = dict()
-            for sensor in msg:
-                if 'lims_field' in sensor.keys() and 'value' in sensor.keys():
-                    lims_field = _lims.endpoint + f'.{sensor["lims_field"]}'
-                    values_to_set[lims_field] = sensor['value']
-                    # values_to_set[sensor['lims_field']] = sensor['value']
-                if 'values' in sensor.keys():
-                    values = sensor['values']
-                    for value in values:
-                        if 'lims_field' in value.keys() and 'value' in value.keys():
-                            lims_field = _lims.endpoint + f'.{value["lims_field"]}'
-                            values_to_set[lims_field] = value['value']
-                            # values_to_set[value['lims_field']] = value['value']
-            if not _lims.engine.set_current_data_values(values_to_set):
-                _lims.logger.debug(f'[LIMS] Failed to set values...\n{values_to_set}')
     
     _lims.logger.debug('[LIMS] Stopped Streaming...')
     while not msgQueue.empty():
